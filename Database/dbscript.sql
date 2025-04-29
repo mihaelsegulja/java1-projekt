212d794755a881495b674cf599bca20ffe1eafeb
mihaelsegulja <mihael.segulja@gmail.com>
2025-04-29 17:44:35 +0200

    Just a few changes

--- Database/dbscript.sql
+++ Database/dbscript.sql
@@ -3,3 +3,27 @@ GO
 
 USE dbMovieManagementSystem
 GO
+
+CREATE TABLE [Actor] (
+	
+)
+GO
+
+CREATE TABLE [Movie] (
+	
+)
+GO
+
+CREATE TABLE [Director] (
+	
+)
+GO
+
+CREATE TABLE [User] (
+	
+)
+GO
+
+CREATE TABLE [Genre] (
+	
+)
---
+++ MovieManagementSystem/Utils/src/main/java/hr/algebra/utils/Utils.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hr.algebra.utils;
+
+/**
+ *
+ * @author MihaelŠegulja
+ */
+public class Utils {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
